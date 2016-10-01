8927e8b63fff9980cad5dffb16469dad539d4c4e
Val <valexx55@gmail.com>
2016-10-02 01:33:15 +0200

    1er commit

---
+++ spring/src/main/resources/archetype-resources/src/test/java/MiControllerTest.java
@@ -0,0 +1,18 @@
+package ${package};
+
+public class MiControllerTest 
+
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MiControllerTest( )
+    {
+        //super( testName );
+    }
+
+  
+}
+
